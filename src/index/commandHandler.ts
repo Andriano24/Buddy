4b264130c003634d338aa1ed4f226991f5085fd1
Andriano <andriano.two@gmail.com>
2022-05-20 20:30:53 +0300

    Added arguments checker to pet calculator command

--- src/index/commandHandler.ts
+++ src/index/commandHandler.ts
@@ -1,6 +1,6 @@
-import { Permissions, Message } from "discord.js";
-import messageContentExtType from "../schemas/messageContentExt";
-import languageType from "../schemas/language";
+import { Permissions } from "discord.js";
+import MessageContentExt from "../types/messageContentExt";
+import Language from "../types/language";
 import { petCalculator } from "../commands/user/petCalculator";
 import languageChange from "../commands/admin/languageChange";
 import prefixChange from "../commands/admin/prefixChange";
@@ -11,14 +11,27 @@ export { petCalculatorCalled };
 
 export default commandHandler;
 
-async function commandHandler(messageContentExt: messageContentExtType) {
+async function commandHandler(messageContentExt: MessageContentExt) {
     var message = messageContentExt.message;
 	var command = messageContentExt.command;
 	var args = messageContentExt.args;
-    var language: languageType = Object.assign({}, messageContentExt.language);
+    var language: Language = Object.assign({}, messageContentExt.language);
 	var prefix = messageContentExt.prefix;
 
     if (command == "petstats" || command == "pet" || command == "stats") {
+        if (args.length != 5) {
+            message.reply({ content: `${language.PetCalculator.wrongFormat[0]}: ${prefix}${language.PetCalculator.wrongFormat[1]}.`, allowedMentions: { repliedUser: false } });
+            return;
+        }
+        if ((+args[0] < 0 || +args[0] > 600) || (+args[1] < 0 || +args[1] > 600) || (+args[2] < 0 || +args[2] > 600) || (+args[3] < 0 || +args[3] > 600) || (+args[4] < 0 || +args[4] > 600)) {
+            message.reply({ content: `${language.PetCalculator.wrongStats}.`, allowedMentions: { repliedUser: false } });
+            return;
+        }
+        if (isNaN(+args[0]) || isNaN(+args[1]) ||isNaN(+args[2]) ||isNaN(+args[3]) ||isNaN(+args[4])) {
+            message.reply({ content: `${language.PetCalculator.wrongFormat[0]}: ${prefix}${language.PetCalculator.wrongFormat[1]}.`, allowedMentions: { repliedUser: false } });
+            return;
+        }
+        
         petCalculatorCalled.push(new petCalculator(messageContentExt));
     }
     else if (command == "language" || command == "lang") {
